025a64f2c2c8d6fa2e085a6e8361ef870326ddc4
oswaldquek <oswald.quek@digital.cabinet-office.gov.uk>
2017-05-22 17:53:21 +0100

    TT-381: Binding form to OnboardingForm class
    still wip

--- app/controllers/environment_config_form_controller.rb
+++ app/controllers/environment_config_form_controller.rb
@@ -1,5 +1,12 @@
 class EnvironmentConfigFormController < ApplicationController
+
   def start
+    @onboarding_form = OnboardingForm.new({})
+    render 'environment_config_form', :layout => 'application'
+  end
+
+  def submit
+    @onboarding_form = OnboardingForm.new(params['onboarding_form'])
     render 'environment_config_form', :layout => 'application'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/' => 'environment_config_form#start'
+  post '/submit' => 'environment_config_form#submit'
 end

bb52d19c276b72a2bc4490e0da8037234c8cbdef
Tuomas Nylund <tunylund@gmail.com>
2017-05-30 16:42:55 +0100

    TT-381: less feature tests, more unit tests
    Converted some unnecessary feature tests to unit tests.
    
    Author: @tunylund

--- spec/models/onboarding_form_spec.rb
+++ spec/models/onboarding_form_spec.rb
@@ -33,16 +33,78 @@ describe OnboardingForm do
       expect(form).to be_valid
     end
 
-    it 'should return false when there are validation errors' do
-      form = OnboardingForm.new({})
-      expect(form).to_not be_valid
-    end
-
-    it 'should require username and password on integration environment' do
+    it 'should require required fields' do
       form = OnboardingForm.new({ environment_access: OnboardingForm::ENVIRONMENT_ACCESS_INTEGRATION })
       form.valid?
-      expect(form.errors['stub_idp_password']).to include("can't be blank")
+      expect(form.errors['service_entity_id']).to include("can't be blank")
+      expect(form.errors['matching_service_entity_id']).to include("can't be blank")
+      expect(form.errors['matching_service_url']).to include("can't be blank")
+      expect(form.errors['matching_service_adapter_ip']).to include("can't be blank")
+      expect(form.errors['testing_devices_ips']).to include("can't be blank")
+      expect(form.errors['service_homepage_url']).to include("can't be blank")
+      expect(form.errors['assertion_consumer_services_https_url']).to include("can't be blank")
+      expect(form.errors['signature_verification_certificate_transaction']).to include("can't be blank")
+      expect(form.errors['signature_verification_certificate_match']).to include("can't be blank")
+      expect(form.errors['encryption_certificate_transaction']).to include("can't be blank")
+      expect(form.errors['encryption_certificate_match']).to include("can't be blank")
+      expect(form.errors['service_display_name']).to include("can't be blank")
+      expect(form.errors['other_ways_display_name']).to include("can't be blank")
+      expect(form.errors['other_ways_complete_transaction']).to include("can't be blank")
       expect(form.errors['stub_idp_username']).to include("can't be blank")
+      expect(form.errors['stub_idp_password']).to include("can't be blank")
+      expect(form.errors['contact_details_name']).to include("can't be blank")
+      expect(form.errors['contact_details_email']).to include("can't be blank")
+      expect(form.errors['contact_details_service']).to include("can't be blank")
+      expect(form.errors['contact_details_department']).to include("can't be blank")
+      expect(form.errors['user_account_creation_uri']).to include("must be a url")
+    end
+
+    it 'should require urls to be urls' do
+      form = OnboardingForm.new({
+        service_entity_id: 'not-a-url',
+        matching_service_entity_id: 'not-a-url',
+        matching_service_url: 'not-a-url',
+        service_homepage_url: 'not-a-url',
+        assertion_consumer_services_https_url: 'not-a-url',
+        user_account_creation_uri: 'not-a-url',
+      })
+      form.valid?
+      expect(form.errors['service_entity_id']).to include('must be a url')
+      expect(form.errors['matching_service_entity_id']).to include('must be a url')
+      expect(form.errors['matching_service_url']).to include('must be a url')
+      expect(form.errors['service_homepage_url']).to include('must be a url')
+      expect(form.errors['assertion_consumer_services_https_url']).to include('must be a url')
+      expect(form.errors['user_account_creation_uri']).to include('must be a url')
+    end
+
+    it 'should not require username and password on production environment' do
+      form = OnboardingForm.new({ environment_access: OnboardingForm::ENVIRONMENT_ACCESS_PRODUCTION })
+      form.valid?
+      expect(form.errors['stub_idp_password']).to_not include("can't be blank")
+      expect(form.errors['stub_idp_username']).to_not include("can't be blank")
+    end
+
+    it 'should validate certificate correctness' do
+      form = OnboardingForm.new({
+        :signature_verification_certificate_transaction => 'malformed-cert',
+        :signature_verification_certificate_match => 'malformed-cert',
+        :encryption_certificate_transaction => 'malformed-cert',
+        :encryption_certificate_match => 'malformed-cert',
+      })
+      form.valid?
+      expect(form.errors['signature_verification_certificate_transaction']).to include("is malformed")
+      expect(form.errors['signature_verification_certificate_match']).to include("is malformed")
+      expect(form.errors['encryption_certificate_transaction']).to include("is malformed")
+      expect(form.errors['encryption_certificate_match']).to include("is malformed")
+    end
+
+    it 'should require service entity id and matching service entity id to be different' do
+      form = OnboardingForm.new({
+        service_entity_id: 'foo',
+        matching_service_entity_id: 'foo',
+      })
+      form.valid?
+      expect(form.errors['matching_service_entity_id']).to include('Entity IDs need to be different')
     end
 
     it 'should require at least 8 chars on password' do

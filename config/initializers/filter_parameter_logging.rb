9ce6542a8479a6f756f7a9c2aa13ccf8cafbdc24
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2018-06-20 17:14:03 +0100

    Filter info that we do not need to store in logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+  Rails.application.config.filter_parameters += [
+                                                 :password,
+                                                 :contact_details_name,
+                                                 :contact_details_email,
+                                                 :contact_details_phone
+                                                ]
